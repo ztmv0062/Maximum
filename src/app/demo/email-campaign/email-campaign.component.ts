2f72a3a1cf61bf5f18a8898c1c6462054f7e9793
Priya <priya.jain@zehntech.com>
2020-01-09 11:36:45 +0530

    data table pagination

--- src/app/demo/email-campaign/email-campaign.component.ts
+++ src/app/demo/email-campaign/email-campaign.component.ts
@@ -1,54 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
 import { MonthlyProfit3 } from './monthly-profit-3';
-import { CoreService } from 'src/app/services/core.service';
+import { CoreService  } from 'src/app/services/core.service';
+
+
+
 
 @Component({
   selector: 'app-email-campaign',
   templateUrl: './email-campaign.component.html',
   styleUrls: ['./email-campaign.component.scss']
 })
-export class EmailCampaignComponent implements OnInit {
+export class EmailCampaignComponent implements OnDestroy , OnInit {
+  dtOptions: DataTables.Settings = {};
   public monthlyProfitChartData3: any;
-  dtRouterLinkOptions: any = {};
+  // dtRouterLinkOptions: any = {};
   public items:[];
   public t : any;
-  constructor(private coreService: CoreService,) { 
+
+  dtTrigger=new Subject();
+
+
+  constructor(private coreService: CoreService,) 
+  { 
     
     this.monthlyProfitChartData3 = MonthlyProfit3.crmChartData;
   }
 
-  ngOnInit() {
-    this.getEmail();
-    
-    this.dtRouterLinkOptions = {
-      ajax: 'fake-data/datatable-data.json',
-      columns: [{
-        title: 'Name',
-        data: 'name'
-      }, {
-        title: 'Position',
-        data: 'position'
-      }, {
-        title: 'Office',
-        data: 'office'
-      }, {
-        title: 'Age',
-        data: 'age'
-      }, {
-        title: 'Start Date',
-        data: 'date'
-      }, {
-        title: 'Salary',
-        data: 'salary'
-      }, {
-        title: 'Action',
-        render: function (data: any, type: any, full: any) {
-          return '<button class="btn btn-outline-primary btn-sm">View</button>';
-        }
-      }],
-      responsive: true
-    };
-  }
+ 
+
+  
 
   getEmail() {
      this.t = localStorage.getItem('name')
@@ -56,14 +37,59 @@ export class EmailCampaignComponent implements OnInit {
     console.log(localStorage.getItem('token') , "hhh")
     this.coreService.get('showallcampaign?api_token='+ localStorage.getItem('token')).subscribe((data: any) => {
       console.log(data)
-  this.items = data;
+    this.items = data;
+    this.dtTrigger.next();
+
+  console.log(this.items)
     }, (error) => {
       console.log(error)
      
     });
   }
-  
+  ngOnInit() {
 
+    this.dtOptions = {
+       pagingType: 'full_numbers',
+       pageLength: 10
+     };
+ 
+     this.getEmail();
+
+    //  this.dtRouterLinkOptions = {
+    //    ajax: 'fake-data/datatable-data.json',
+    //    columns: [{
+    //      title: 'Name',
+    //      data: 'name'
+    //    }, {
+    //      title: 'Position',
+    //      data: 'position'
+    //    }, {
+    //      title: 'Office',
+    //      data: 'office'
+    //    }, {
+    //      title: 'Age',
+    //      data: 'age'
+    //    }, {
+    //      title: 'Start Date',
+    //      data: 'date'
+    //    }, {
+    //      title: 'Salary',
+    //      data: 'salary'
+    //    }, {
+    //      title: 'Action',
+    //      render: function (data: any, type: any, full: any) {
+    //        return '<button class="btn btn-outline-primary btn-sm">View</button>';
+    //      }
+    //    }],
+    //    responsive: true
+    //  };
+    
+   }
+  
+  ngOnDestroy() {
+    // Do not forget to unsubscribe the event
+    this.dtTrigger.unsubscribe();
+  }
 }
 
 
